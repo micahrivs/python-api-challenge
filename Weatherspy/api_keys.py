da8cb8b22b979a78c5886514608ce0e52fee6dc9
micsrivs <micahgalbadores@Micahs-MacBook-Pro.local>
2023-05-13 15:55:23 -0700

    final

--- Weatherspy/api_keys.py
+++ Weatherspy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "5c3c969dcd07ac1b4a0d92809f40db8e"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "7f84bf2985834d909dd744227c93edc7"
